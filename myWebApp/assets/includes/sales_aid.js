deb5b64a4e61f3feb106f525e0a72cd650a4f131
mwetmor <145296572+mwetmor@users.noreply.github.com>
2023-09-17 21:41:22 -0400

    Update sales_aid.js

--- myWebApp/assets/includes/sales_aid.js
+++ myWebApp/assets/includes/sales_aid.js
@@ -1,21 +1,61 @@
 /*  JavaScript Document  */
 
+var eventType = 'click';
+
+/* IScroll */
+var myScroll;
+function loaded(){
+    myScroll = new IScroll('#scroll',{checkDOMChanges:true});
+}
+document.addEventListener('DOMContentLoaded', loaded, false);
+/* end IScroll */
+
 $(document).ready(function(){
 
     updateOrientationListener();
 
-    $('nav a').on('click',function(){
+    window.setTimeout(startMap,1500);
+
+    $('nav a').on(eventType,function(){
         var pageToLoad = $(this).attr('data-file');
         changePage(pageToLoad);
         $('nav a').removeClass('selected');
         $(this).addClass('selected');
     });
+    $('nav a:nth-child(1)').trigger(eventType);
 
 });
 
+function startMap(){
+    var latlng = new google.maps.LatLng(28.614367, -80.602991); 
+    var myOptions = {zoom: 16, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP}; 
+    var map = new google.maps.Map(document.getElementById('map_canvas'),myOptions); 
+    var marker = new google.maps.Marker({
+	position: latlng, 
+	map: map,
+	title:'Office Location'
+});
+
+}
+
 function changePage(fileName){
     $('.content_container').animate({opacity:0},500,function(){
+        //detect homepage
+        if( fileName == 'home.html'){
+            $('.page').addClass('home');
+        }else{
+            $('.page').removeClass('home');
+        }
+        // detect contact
+        if( fileName == 'contact_us.html'){
+            $('.map_container').addClass('on').removeClass('off');
+        }else{
+            $('.map_container').addClass('off').removeClass('on');
+        }
+
+        // load content
         $('.content_loading_container').load('assets/content/'+fileName,function(){
+            myScroll.refresh();
             $('.content_container').animate({opacity:1},500);
         });
     });
